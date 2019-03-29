ac8b48b5c8216522b4e4e98d0bf2a7c4c7d63190
Matias Borghi <borghi.matias@gmail.com>
2019-03-29 15:14:52 -0300

    added tables with other material ui components and sliding panes

---
+++ material-ui-tables/src/splitpane.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SplitPane from 'react-split-pane';
+import './index.css'
+import './splitpane.css'
+import  NestedList from './Components/NestedList/nested-list';
+import ExpansionPanel from './Components/Accordion/accordion';
+import FullWidthTabs from './Components/Tabs/tabs';
+import EnhancedTable from './Components/Table/table';
+import ReactVirtualizedTable from './Components/Table/virtual-table';
+
+class SplitPanes extends React.Component {
+    render() {
+        return (
+            <div>
+                <SplitPane split="vertical" minSize={200} maxSize={300} defaultSize="15%">
+                    <div>
+                        <NestedList />
+                    </div>
+                    <div>
+                        <SplitPane split="vertical" defaultSize="20%" primary="second" allowResize={false}>
+                            <div>
+                                <SplitPane split="horizontal" defaultSize="20%" primary="second" minSize={150} maxSize={300}>
+                                    <div>
+                                        
+                                            <ReactVirtualizedTable/>
+                                                
+                                    </div>
+                                    <div>
+                                        <FullWidthTabs />
+                                    </div>
+                                    
+                                </SplitPane>
+                            </div>
+                            <div>
+                                <ExpansionPanel />
+                            </div>
+                        </SplitPane>
+                    </div>
+                </SplitPane>
+            </div>
+        );
+    }
+}
+
+const content = document.getElementById('pane');
+ReactDOM.render(<SplitPanes/>, content);
