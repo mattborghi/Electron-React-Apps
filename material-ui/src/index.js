8167e6f7ba82e5826fcf381bcd42d59db211aa73
Matias Borghi <borghi.matias@gmail.com>
2019-03-28 23:53:25 -0300

    added appbar to material ui

--- material-ui/src/index.js
+++ material-ui/src/index.js
@@ -2,6 +2,22 @@ import './index.css';
 import './splitpane.js';
 import * as serviceWorker from './serviceWorker';
 
+import SimpleAppBar from './Components/AppBar/appbar';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AppBar extends React.Component {
+    render() {
+        return(
+            <div>
+                <SimpleAppBar/>
+            </div>
+        );
+    }
+}
+const content = document.getElementById('appbar');
+ReactDOM.render(<AppBar/>, content);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
