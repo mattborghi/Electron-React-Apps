bb247807646adc0211c1d4c60d5fdea4037b8b66
Matias Borghi <borghi.matias@gmail.com>
2019-04-05 12:26:39 -0300

    react splitter with a bettwer splitter panels

---
+++ react-splitter/src/splitpane.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import SplitPane from 'react-split-pane';
+import './index.css';
+// import './splitpane.css';
+import  NestedList from './Components/NestedList/nested-list';
+import ExpansionPanel from './Components/Accordion/accordion';
+import FullWidthTabs from './Components/Tabs/tabs';
+import SplitterLayout from 'react-splitter-layout';
+import 'react-splitter-layout/lib/index.css';
+
+
+
+class SplitPanes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleSidebar = this.toggleSidebar.bind(this);
+        this.state = {
+          sidebarVisible: true
+        };
+      }
+    
+      toggleSidebar() {
+        this.setState(state => ({ sidebarVisible: !state.sidebarVisible }));
+      }
+
+    render() {
+        return (
+            <div>
+                <SplitterLayout primaryIndex={0} primaryMinSize={10} secondaryMinSize={80} percentage>
+                    {/* Hiding sidebar */}
+                    {this.state.sidebarVisible &&
+                    (
+                        <div>
+                            <NestedList />
+                        </div>
+                    )}
+                    <div>
+                        <SplitterLayout primaryIndex={0} primaryMinSize={80} secondaryMinSize={20} percentage secondaryInitialSize={20}>
+                            <div>
+                                <SplitterLayout vertical percentage primaryIndex={0} secondaryInitialSize={20} primaryMinSize={60} secondaryMinSize={10}>
+                                    <div>
+                                        1st horizontal
+                                    </div>
+                                    <div>
+                                        <FullWidthTabs />
+                                    </div>
+                                </SplitterLayout>
+                            </div>
+                            <div>
+                                <button type="button" onClick={this.toggleSidebar}>
+                                    {this.state.sidebarVisible && 'Hide Sidebar'}
+                                    {!this.state.sidebarVisible && 'Show Sidebar'}
+                                </button>
+                                <ExpansionPanel />
+                            </div>
+                        </SplitterLayout>
+                    </div>
+                </SplitterLayout>
+            </div>
+        );
+    }
+}
+
+const content = document.getElementById('pane');
+ReactDOM.render(<SplitPanes/>, content);
