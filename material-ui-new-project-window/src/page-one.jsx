2533f72f5d776f38a27de7368f6be0974a263e76
Matias Borghi <borghi.matias@gmail.com>
2019-04-25 19:00:21 -0300

    new project window

---
+++ material-ui-new-project-window/src/page-one.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import UniversalMC from './img/universal-mc.png'
+import Grid from '@material-ui/core/Grid'
+
+function PageOne() {
+
+  const style = {
+    color: '#ff5722',
+    width: 500,
+    height: 100,
+    fontSize: 30,
+    marginTop: 200,
+    // textColor: '#ff5722',
+    // backgroundColor: '#ff5722',
+  }
+
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+    typography: { 
+       useNextVariants: true
+    }
+  });
+
+  return (
+    <MuiThemeProvider theme = { theme }>
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+
+      <div style={{marginTop: 100}}>
+        <img src={UniversalMC} alt="universal-mc-logo" height={400} ></img>
+      </div>
+
+
+      <Grid container
+        justify="space-evenly"
+        alignItems="center"
+        direction="row"
+      >
+        <Button variant="outlined" size="large" style={style}>
+          New Project
+        </Button>
+        <Button variant="outlined" size="large" style={style}>
+          Load Project
+        </Button>
+      </Grid>
+      <Button variant="outlined" size="large">
+          Checkout version 2!
+      </Button>
+    </Grid>
+
+    </MuiThemeProvider>
+  );
+}
+
+
+
+export default PageOne;
