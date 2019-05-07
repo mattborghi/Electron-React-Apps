d187621c94af80c1c54292aeca9dcbb91cd3c357
Matias Alejandro Febles <MatiasF@ad.crisil.com>
2019-05-07 13:21:25 -0300

    :construction: Adding files

--- material-ui-tables/src/Components/Table/material-table.js
+++ material-ui-tables/src/Components/Table/material-table.js
@@ -16,22 +16,15 @@ class NewTable extends Component {
       <div style={{ maxWidth: '100%' }}>
         <MaterialTable
         columns={[
-            {
-            title: 'Name',
-            render: rowData => rowData.name + ' ' + rowData.surname,
-            },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa', 10: 'San Martin City'},
-            },
+            {title: 'Test Name', render: rowData => rowData.name + ' ' + rowData.surname,},
+            // {title: 'Birth Year',  field: 'birthYear', type: 'numeric' },
+            {title: 'Contract', field: 'contract', lookup: { 1: 'Contract A', 2: 'Contract B', 3: 'Contract C'},},
             {
             title: 'Progress',
             field: 'successScore',
             render: rowData => {
                 const score = rowData.successScore + '%'
-                const color = rowData.successScore > 70 ? '#4CAF50' : '#f44336'
+                const color = rowData.successScore > 25 ? '#4CAF50' : '#f44336'
                 return (
                 <div style={{ width: '100%', backgroundColor: '#ddd', height: 20 }}>
                     <div
@@ -55,128 +48,57 @@ class NewTable extends Component {
             {
             name: 'One Dimension',
             surname: 'Plot',
-            birthYear: 1987,
-            birthCity: 63,
-            successScore: 58,
+            // birthYear: 1987,
+            contract: 1,
+            successScore: 20,
             plot: <OneDimension /> ,
             },
             {
             name: 'Contour',
             surname: 'Plot',
-            birthYear: 2017,
-            birthCity: 34,
+            // birthYear: 2017,
+            contract: 2,
             successScore: 90,
             plot: <Contourplot /> ,
             },
             {
             name: 'Comparison',
             surname: 'Plot',
-            birthYear: 1986,
-            birthCity: 10,
+            // birthYear: 1986,
+            contract: 3,
             successScore: 70,
             plot: <DiffPlot /> ,
             },
         ]}
+        
         title="Custom Render Example"
-        // actions={[
-        //     {
-        //        icon: 'search', 
-        //        tooltip: 'Preview File',
-        //        onClick: (event, rowData) => {
-        //             // alert('Previewing file ' + rowData.name)
-        //        },
-        //        iconProps: {
-        //             style: {
-        //                 fontSize: 30,
-        //                 // color: 'blue',
-        //             },
-        //        },
-        //     },
-        // ]}
 
         detailPanel={[
-            // {
-            //     tooltip: 'Show Row info',
-            //     render: rowData => {
-            //     return (
-            //         <div style={{fontSize: 100,textAlign: 'center',color: 'white',backgroundColor: '#43A047',}}>
-            //         {rowData.birthYear}                    
-            //         </div>
-            //     )
-            //     },              
-            // //     render: () => {
-            // //         alert('Previewing file ')
-            // //    },
-            // },
-
             {
-                icon: 'search',
+                icon: 'favorite_border',
                 tooltip: 'Show Preview',
                 render: rowData => {
                 return (
                     <div>               
                         {rowData.plot}  
-                        {/* <CircularIntegration /> */}
-                        {/* <PDFFile /> */}
-                        {/* <spdf.SimplePDF file="./test.pdf"/> */}
                     </div>
                 )
                 },
             },
+        ]}    
 
-            // rowData => ({
-            //     disabled: rowData.name === 'ax',
-            //     icon: 'favorite_border',
-            //     openIcon: 'favorite',
-            //     tooltip: 'Show Both',
-            //     render: rowData => {
-            //     return (
-            //         <div style={{fontSize: 100,textAlign: 'center',color: 'white',backgroundColor: '#FDD835',}}>
-            //         {rowData.name} {rowData.surname}
-            //         </div>
-            //     )
-            //     },
-            // }),
-        ]}
+      
 
-        editable={{
-        // onRowAdd: newData =>
-        //     new Promise((resolve, reject) => {
-        //     setTimeout(() => {
-        //         {
-        //     //     /* const data = this.state.data;
-        //     // data.push(newData);
-        //     // this.setState({ data }, () => resolve()); */
-        //         }
-        //         resolve()
-        //     }, 1000)
-        //     }),
-        onRowUpdate: (newData, oldData) =>
-            new Promise((resolve, reject) => {
-            setTimeout(() => {
-                // {
-                //         /* const data = this.state.data;
-                // const index = data.indexOf(oldData);
-                // data[index] = newData;                
-                // this.setState({ data }, () => resolve()); */
-                // }
-                resolve()
-            }, 1000)
-            }),
-        onRowDelete: oldData =>
-            new Promise((resolve, reject) => {
-            setTimeout(() => {
-                // {
-                //         /* let data = this.state.data;
-                // const index = data.indexOf(oldData);
-                // data.splice(index, 1);
-                // this.setState({ data }, () => resolve()); */
-                // }
-                resolve()
-            }, 1000)
-            }),
-          }}
-          onRowClick={(event, rowData, togglePanel) => togglePanel(1)}
+        // editable={{
+
+        // onRowUpdate: (newData, oldData) =>
+        //     new Promise((resolve, reject) => {setTimeout(() => {resolve()}, 1000)}),
+
+        // onRowDelete: oldData =>
+        //     new Promise((resolve, reject) => {setTimeout(() => {resolve()}, 1000)}),
+        // }}
+          
+        onRowClick={(event, rowData, togglePanel) => togglePanel(1)}
         />
       </div>
     )
