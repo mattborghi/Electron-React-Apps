ee132573693420ae32a464e880de330a603b718d
Matias Borghi <borghi.matias@gmail.com>
2019-05-08 10:27:46 -0300

    :white_check_mark: Fixed moving pane with icons

--- inherent-risk/src/Components/Accordion/accordion.js
+++ inherent-risk/src/Components/Accordion/accordion.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import React, { useState } from 'react'
+// import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
-import { withStyles } from '@material-ui/styles';
-import MovePaneIcon from '../icons/move-pane'
-import UpArrowIcon from '../icons/up-arrow'
+import { withStyles } from '@material-ui/styles'
+import Typography from '@material-ui/core/Typography'
+// import MovePaneIcon from '../icons/move-pane'
+import MoveWindowIcon from '../icons/move-window'
+// import UpArrowIcon from '../icons/up-arrow'
 import SaveFileIcon from '../icons/save-file'
 import CalculateIcon from '../icons/calculate'
-import GenerateIcon from '../icons/generate'
+// import GenerateIcon from '../icons/generate'
+import ContractIcon from '../icons/contract'
+import CloudComputingLogo from '../icons/cloud-computing'
+// import BlueprintIcon from '../icons/blueprint'
 
-const black="#424242"
+// const black="#424242"
 const white="#ffffff"
 
 const StyledButton = styled(Button)`
@@ -30,7 +35,12 @@ const styles = {
     bottom: 0,
     left: 0,
     position: "absolute",
-  }
+  },
+icon: {
+  width: 15,
+  height: 15,
+  marginBottom: -2
+}
 }
 
 function CustomizedExpansionPanel(props) {
@@ -46,31 +56,33 @@ function CustomizedExpansionPanel(props) {
 
   return (
     <div>
-      {/* Move pane button */}
-      <StyledButton onClick={() => movePane()} >
-        <MovePaneIcon style={{position: "relative", top: 0, left: 0}} />
-      </StyledButton>
-
       {/* Buttons */}
-    
       <StyledButton color={props.bgColor} text={props.textColor} className={classes.root} >
-        Save
-        <div style={{ marginRight: 20}}><SaveFileIcon fill={white}/></div>
+        <div style={{paddingRight: 25, marginTop: 4, left: 10}} ><SaveFileIcon fill={white} /></div>
+        <Typography style={{textTransform: "none", fontSize: 20, fontFamily: 'Open Sans',}}>Save</Typography>
+      </StyledButton>
+
+  
+      <StyledButton color={props.bgColor} text={props.textColor} className={classes.root} >
+        <div style={{paddingRight: 25, marginTop: 4}} ><CloudComputingLogo fill={white} /></div>
+        <Typography style={{textTransform: "none", fontSize: 20, fontFamily: 'Open Sans',}}>Load</Typography>
       </StyledButton>
 
       <StyledButton color={props.bgColor} text={props.textColor} className={classes.root} >
-        Load
-        <div style={{ marginRight: 20}}><UpArrowIcon fill={white}/></div>
+        <div style={{paddingRight: 25, marginTop: 4}} ><CalculateIcon fill={white} /></div>
+        <Typography style={{textTransform: "none", fontSize: 20, fontFamily: 'Open Sans',}}>Calculate Risk</Typography>
       </StyledButton>
 
       <StyledButton color={props.bgColor} text={props.textColor} className={classes.root} >
-        Calculate Risk
-        <div style={{marginRight: 20}}><CalculateIcon fill={white}/></div>
+        <div style={{paddingRight: 25, marginTop: 4}} ><ContractIcon fill={white} /></div>
+        <Typography style={{textTransform: "none", fontSize: 20, fontFamily: 'Open Sans',}}>Generate Report</Typography>
       </StyledButton>
 
-      <StyledButton color={props.bgColor} text={props.textColor} className={classes.root} >
-        Generate Report
-        <div style={{marginRight: 20}}><GenerateIcon fill={white}/></div>
+
+      {/* Move pane button */}
+      <StyledButton onClick={() => movePane()} style={{position: "absolute", bottom: 0, left: 0}}>
+        <div style={{paddingRight: 25, marginTop: 4,}} ><MoveWindowIcon fill={white} className={classes.icon}/></div>
+        <Typography style={{textTransform: "none", fontSize: 15, fontFamily: 'Open Sans',}}>Move Pane</Typography>
       </StyledButton>
       
     </div>
