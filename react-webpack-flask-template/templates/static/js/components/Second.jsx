3ebfda1c26a7f43772ecbfaf6004068119e9db00
Matias Borghi <borghi.matias@gmail.com>
2019-05-14 12:17:51 -0300

    :alembic: adding another component

---
+++ react-webpack-flask-template/templates/static/js/components/Second.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class Second extends Component {
+    render() {
+       return (
+          <h1>Hello from Second Page!</h1>
+       )
+    }
+}
