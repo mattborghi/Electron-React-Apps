f2a506c42d997bcb0a4f55120168b8a8db90ae72
Matias Alejandro Febles <MatiasF@ad.crisil.com>
2019-05-16 09:52:10 -0300

    working with material-table and nested-list in order to share information between classes

--- material-ui-tables/src/splitpane.js
+++ material-ui-tables/src/splitpane.js
@@ -5,6 +5,7 @@ import SplitPane from 'react-split-pane';
 import './index.css'
 import './splitpane.css'
 import  NestedList from './Components/NestedList/nested-list';
+import  NestedList2 from './Components/NestedList/nested-list2';
 import ExpansionPanel from './Components/Accordion/accordion';
 import FullWidthTabs from './Components/Tabs/tabs';
 // import EnhancedTable from './Components/Table/table';
@@ -12,22 +13,54 @@ import FullWidthTabs from './Components/Tabs/tabs';
 import NewTable from './Components/Table/material-table';
 
 class SplitPanes extends React.Component {
+
+    constructor(props) {
+        super(props);
+           this.state = {
+           checkboxState: new Map(),
+        };
+    }
+
+    onTest = (recieveChecks) => {this.setState(state => ({checkboxState: recieveChecks}))}
+    Onchecked = (recieveChecks) => {this.setState(state => ({checkboxState: recieveChecks}))}
+
     render() {
+        // console.log (this.state.checkboxState)
+        const checkboxes = [
+            {
+              name: 'One_Dimension',
+              feature: 'checkBox1',
+              label: 'Check Box 1',
+            },
+            {
+              name: 'Contour',
+              feature: 'checkBox2',
+              label: 'Check Box 2',
+            },
+            {
+              name: 'Comparison',
+              feature: 'checkBox3',
+              label: 'Check Box 3',
+            },
+            // {
+            //   name: 'check-box-4',
+            //   feature: 'checkBox4',
+            //   label: 'Check Box 4',
+            // },
+        ];
         return (
             <div>
                 <SplitPane split="vertical" minSize={200} maxSize={300} defaultSize="15%">
                     <div>
-                        <NestedList />
+                        {/* <NestedList test={this.onTest}/> */}
+                        <NestedList2 checked={this.Onchecked} data={checkboxes} />
                     </div>
                     <div>
                         <SplitPane split="vertical" defaultSize="20%" primary="second" allowResize={false}>
                             <div>
                                 <SplitPane split="horizontal" defaultSize="20%" primary="second" minSize={150} maxSize={300}>
-                                    <div>
-                                            {/* <EnhancedTable/> */}
-                                            {/* <ReactVirtualizedTable/> */}
-                                            <NewTable/>
-                                                
+                                    <div>                                 
+                                            <NewTable rowState={this.state.checkboxState} data={checkboxes}/>                                                
                                     </div>
                                     <div>
                                         <FullWidthTabs />
